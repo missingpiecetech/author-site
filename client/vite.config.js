aaedbe342b443b07cd3514cd66a405a3924a06b0
missingpiecetech <jennifer@missingpiecetech.com>
2026-03-02 01:33:45 -0600

    fix path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { createSquareApiPlugin } from "./server/squareApiPlugin";
+import { createSquareApiPlugin } from "./api/squareApiPlugin";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
